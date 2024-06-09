c9ccd9ca0c957dcbda2a60f2475872887ee37ab5
hmccarthy1 <huntermccarthy56@hotmail.com>
2024-06-09 10:21:17 -0500

    Add sidebar images and links as well as sidebar header logo

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,13 +1,18 @@
-
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+    
+const loggedIn = {firstName: 'Hunter', lastName: 'McCarthy'}
+
   return (
-    <main>
-        Sidebar
+    
+
+    <main className=" flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
         {children}
     </main>
   );
